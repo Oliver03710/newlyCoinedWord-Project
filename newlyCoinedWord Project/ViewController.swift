8f6b77eb8f1ce92b6c096e76c7e5a3c7d311932c
Oliver <jhee2619@gmail.com>
2022-07-10 13:52:40 +0900

    앱 아이콘 추가 / 기본 오토레이아웃 설정

--- newlyCoinedWord Project/ViewController.swift
+++ newlyCoinedWord Project/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    
+    
+    
+    
+    // MARK: - Init
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        
     }
 
+    // MARK: - Helper Functions
+    
+    
+    
 
 }
 
